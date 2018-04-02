32b4cc79af772f90283328ae24fb769ef40222e6
Anthony Smith <a88smith@gmail.com>
2018-04-01 22:01:33 -0700

    adds packages and base fragment class. adds dependencies.

--- app/src/main/java/com/droidsmith/redditlog/view/MainActivity.kt
+++ app/src/main/java/com/droidsmith/redditlog/view/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.droidsmith.redditlog
+package com.droidsmith.redditlog.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.droidsmith.redditlog.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/droidsmith/redditlog/view/fragments/PopularFragment.kt
@@ -0,0 +1,12 @@
+package com.droidsmith.redditlog.view.fragments
+
+
+class PopularFragment : BaseFragment(){
+
+    private lateinit var instance: PopularFragment
+
+    companion object {
+        //lookup the singleton class agin
+    }
+
+}
